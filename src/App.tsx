fad41debd67626e65be6ab344541ec4ae3c3c6ef
Nahuel Orselli <103908691+NahuelOrselli@users.noreply.github.com>
2026-05-15 10:27:21 +0200

    feat: :sparkles: add important info modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { FullscreenLoading } from './components/ui/fullscreen-loading'
 import { useAuth } from './auth/AuthContext'
+import { ImportantInfoModal } from './modules/components/ImportantInfoModal'
 import { DashboardProvider } from './modules/context/DashboardContext'
 import { DashboardLayout } from './modules/layout/DashboardLayout'
 import {
@@ -60,53 +61,56 @@ function DashboardShell() {
 
 export function App() {
   return (
-    <Routes>
-      <Route path='/reset-password' element={<ResetPasswordPage />} />
+    <>
+      <ImportantInfoModal />
+      <Routes>
+        <Route path='/reset-password' element={<ResetPasswordPage />} />
 
-      <Route element={<PublicOnlyRoute />}>
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-        <Route path='/forgot-password' element={<ForgotPasswordPage />} />
-      </Route>
+        <Route element={<PublicOnlyRoute />}>
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/forgot-password' element={<ForgotPasswordPage />} />
+        </Route>
 
-      <Route element={<PrivateRoute />}>
-        <Route element={<DashboardShell />}>
-          <Route index element={<HomePage />} />
-          <Route path='new-ica-words' element={<NewIcaWordsPage />} />
-          <Route path='my-ica-words' element={<MyIcaWordsPage />} />
-          <Route path='flashcards' element={<FlashcardsPage />} />
-          <Route path='flashcards/play/:mode' element={<FlashcardsPlayPage />} />
-          <Route path='activation-phrase' element={<ActivationPhrasePage />} />
-          <Route path='phrase-history' element={<PhraseHistoryPage />} />
-          <Route path='master-notes' element={<MasterNotesPage />} />
-          <Route path='master-notes/note/:noteId' element={<MasterNoteDetailPage />} />
-          <Route
-            path='master-notes/note/:noteId/activate/:phraseId'
-            element={<MasterNoteActivatePhrasePage />}
-          />
-          <Route path='leaderboard' element={<LeaderboardPage />} />
-          <Route path='streaks' element={<StreaksPage />} />
-          <Route path='profile' element={<ProfilePage />} />
-          <Route path='trackers' element={<TrackersPage />} />
-          <Route path='trackers/new' element={<NewTrackerPage />} />
-          <Route path='trackers/:trackerId' element={<TrackerDetailPage />} />
-          <Route element={<AnalyticsAdminRoute />}>
-            <Route path='analytics' element={<AnalyticsPage />} />
-          </Route>
-          <Route element={<CoachingMemberRoute />}>
-            <Route path='coaching-personalized' element={<CoachingPersonalizedPage />} />
-          </Route>
-          <Route element={<CoachingAdminRoute />}>
-            <Route path='manage-coaching' element={<ManageCoachingPage />} />
-            <Route path='manage-coaching/:userId' element={<ManageCoachingUserPage />} />
-          </Route>
-          <Route element={<SuperAdminRoute />}>
-            <Route path='manage-whitelist' element={<ManageWhitelistPage />} />
-            <Route path='historic-leaderboard' element={<HistoricLeaderboardPage />} />
+        <Route element={<PrivateRoute />}>
+          <Route element={<DashboardShell />}>
+            <Route index element={<HomePage />} />
+            <Route path='new-ica-words' element={<NewIcaWordsPage />} />
+            <Route path='my-ica-words' element={<MyIcaWordsPage />} />
+            <Route path='flashcards' element={<FlashcardsPage />} />
+            <Route path='flashcards/play/:mode' element={<FlashcardsPlayPage />} />
+            <Route path='activation-phrase' element={<ActivationPhrasePage />} />
+            <Route path='phrase-history' element={<PhraseHistoryPage />} />
+            <Route path='master-notes' element={<MasterNotesPage />} />
+            <Route path='master-notes/note/:noteId' element={<MasterNoteDetailPage />} />
+            <Route
+              path='master-notes/note/:noteId/activate/:phraseId'
+              element={<MasterNoteActivatePhrasePage />}
+            />
+            <Route path='leaderboard' element={<LeaderboardPage />} />
+            <Route path='streaks' element={<StreaksPage />} />
+            <Route path='profile' element={<ProfilePage />} />
+            <Route path='trackers' element={<TrackersPage />} />
+            <Route path='trackers/new' element={<NewTrackerPage />} />
+            <Route path='trackers/:trackerId' element={<TrackerDetailPage />} />
+            <Route element={<AnalyticsAdminRoute />}>
+              <Route path='analytics' element={<AnalyticsPage />} />
+            </Route>
+            <Route element={<CoachingMemberRoute />}>
+              <Route path='coaching-personalized' element={<CoachingPersonalizedPage />} />
+            </Route>
+            <Route element={<CoachingAdminRoute />}>
+              <Route path='manage-coaching' element={<ManageCoachingPage />} />
+              <Route path='manage-coaching/:userId' element={<ManageCoachingUserPage />} />
+            </Route>
+            <Route element={<SuperAdminRoute />}>
+              <Route path='manage-whitelist' element={<ManageWhitelistPage />} />
+              <Route path='historic-leaderboard' element={<HistoricLeaderboardPage />} />
+            </Route>
           </Route>
         </Route>
-      </Route>
-      <Route path='*' element={<RootRedirect />} />
-    </Routes>
+        <Route path='*' element={<RootRedirect />} />
+      </Routes>
+    </>
   )
 }
